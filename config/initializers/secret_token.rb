936b27226fd53ee7d17ce469de4a1077bf0a7658
Ryan Devlin <saigonosu@gmail.com>
2014-04-16 09:59:59 -0400

    Initial commit. First Chapter in Agile Web Development with Rails 4. Generate scaffold, migrate products db, edit scss and products/index.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'd3e69bdf1eeafea44d22b28d186047663464fbecd289f4f7ee137caa1f7b88f5fb3c7b5f1ff9c521a6f4ecd93061a9574d4d7f706c96f6f6bc4b5c1dc39a741d'
